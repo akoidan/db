73e7005355b97c5ef083ec0ea781f478b60a6ed0
akoidan <deathangel908@gmail.com>
2026-02-06 05:35:16 +0100

    f

--- setup_mongo.sh
+++ setup_mongo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Then reconfigure with container names
-mongosh --host localhost:28018 -u admin -p password --authenticationDatabase admin --eval "
+mongosh --host localhost:28018 --eval "
+use admin
 rs.initiate({
   _id: 'rs0',
   members: [
@@ -9,5 +10,11 @@ rs.initiate({
     { _id: 1, host: 'localhost:28019', priority: 1 },
     { _id: 2, host: 'localhost:28020', priority: 1 }
   ]
+});
+db.createUser({
+  user: 'admin',
+  pwd: 'password',
+  roles: [{ role: 'root', db: 'admin' }]
 })
 "
+
